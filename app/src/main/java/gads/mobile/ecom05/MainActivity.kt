4947b7f0f73ffa181cb65c57e82b61c0592623a1
Sylvester Akwaowo <ultrasoft101@gmail.com>
2020-10-12 04:44:30 +0000

    Init Project structure

---
+++ app/src/main/java/gads/mobile/ecom05/MainActivity.kt
@@ -0,0 +1,11 @@
+package gads.mobile.ecom05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
