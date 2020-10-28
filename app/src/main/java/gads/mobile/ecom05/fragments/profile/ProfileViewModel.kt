16e85178a318a1e79e400907171102a8b7805c61
arnaudram <52896739+arnaudram@users.noreply.github.com>
2020-10-28 16:40:17 +0200

    user profile implementation v:1.0

--- app/src/main/java/gads/mobile/ecom05/fragments/profile/ProfileViewModel.kt
+++ app/src/main/java/gads/mobile/ecom05/fragments/profile/ProfileViewModel.kt
@@ -1,7 +1,26 @@
 package gads.mobile.ecom05.fragments.profile
 
 import androidx.lifecycle.ViewModel
+import gads.mobile.ecom05.repositories.UserCredentialRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
 
-class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ProfileViewModel(private val userCredentialRepository: UserCredentialRepository) :
+    ViewModel() {
+
+
+    fun getCurrentUser() = userCredentialRepository.getCurrentUser()
+    fun signCurrentUserOut() {
+        userCredentialRepository.signOutUser()
+    }
+
+    fun updateEmail(email: String) {
+        userCredentialRepository.updateProfileEmail(email)
+    }
+
+    fun updatePassword(passWord: String) {
+        userCredentialRepository.updateProfilePassWord(passWord)
+    }
 }
